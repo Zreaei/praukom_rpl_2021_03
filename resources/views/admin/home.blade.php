6186a91cb9ad1c1e38cd78b631de302654b19454
Zreaei <zenarthxdgt@gmail.com>
2022-11-16 11:34:57 +0700

    New

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('container')
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quae eius fuga? Impedit maxime voluptas a quas aut! Quo quos animi rerum laudantium ducimus amet pariatur possimus sapiente labore?</h1>
+@endsection
