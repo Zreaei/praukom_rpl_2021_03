8b9b1e2eed7734e52ffc748290aa9385e74b9769
Zreaei <zenarthxdgt@gmail.com>
2022-11-28 08:38:15 +0700

    Tambah views admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,6 @@ class AdminController extends Controller
 {
     public function home() 
     {
-        return view('home');
+        return view('admin.home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome/welcome');
+    return view('layouts/app');
 });
 
 Route::get('/adminhome', [AdminController::class, 'home']);
