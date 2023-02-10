6bc2c68e0b5cbb8bc7db921c912c8504b69ece2b
Zreaei <zenarthxdgt@gmail.com>
2023-02-11 06:40:58 +0700

    Pushing Before Making Login

--- resources/views/admin/data-op/data-op.blade.php
+++ resources/views/admin/data-op/data-op.blade.php
@@ -8,7 +8,6 @@
         <thead>
             <tr>
                 <th class="text-center bg-blue-700 text-white">id_operator</th>
-                <th class="text-center bg-blue-700 text-white">Nama Operator</th>
                 <th class="text-center bg-blue-700 text-white">Opsi</th>
             </tr>
         </thead>
@@ -17,7 +16,6 @@
             <!-- row 1 -->
             <tr>
                 <td class="text-center bg-white"><h1>{{ $item->id_operator }}</h1></td>
-                <td class="text-center bg-white"><h1>{{ $item->nama_operator }}</h1></td>
                 <td class="text-center bg-white">
                     <a href="data-op/edit/{{ $item->id_operator }}"><button class="btn btn-warning">Edit</button></a>
                     <a href="data-op/hapus/{{ $item->id_operator }}"><button class="btn btn-error">Hapus</button></a>
